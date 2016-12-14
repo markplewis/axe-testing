a5877532a093525f152b14cf7c1885535f9b8507
markplewis <markplewis1@gmail.com>
2016-12-14 17:00:07 -0500

    Reworks the tests some more

--- test/axe-test.js
+++ test/axe-test.js
@@ -5,7 +5,7 @@ if (!process.env.URL) {
 const url = process.env.URL;
 const browser = "firefox"; // "firefox" or "chrome" (which has problems)
 
-const { Builder, By, chrome, firefox } = require('selenium-webdriver');
+const { Builder, By, chrome, firefox, until, promise } = require('selenium-webdriver');
 const AxeBuilder = require('axe-webdriverjs');
 const assert = require('assert');
 
@@ -21,7 +21,12 @@ describe('Accessibility', function() {
       .build();
 
     driver.get(url).then(function() {
-      done();
+      driver.wait(until.elementIsVisible(
+        driver.findElement(By.tagName("h1"))
+      ), 10000)
+      .then(function() {
+        done();
+      });
     });
   });
 
@@ -30,34 +35,167 @@ describe('Accessibility', function() {
       done();
     });
   });
-
+  
+  it('title should exist', function(done) {
+    driver.wait(driver.findElement(By.css("title")).then(
+      function(webElement) {
+        console.log("title exists");
+        webElement.getText().then(function(text) {
+          console.log(text);
+        });
+      },
+      function(err) {
+        throw new Error("title does not exist");
+      }
+    ), 10000)
+    .then(function() {
+      done();
+    });
+  });
+  
   it('should analyze the page with aXe', function(done) {
-    AxeBuilder(driver)
-      .analyze(function(results) {
-        console.log(`Accessibility Violations: ${results.violations.length}`);
-        if (results.violations.length > 0) {
-          results.violations.forEach((violation, index) => {
-            console.log(`${index + 1}. ${violation.help}`);
-          });
-        }
-        assert.equal(results.violations.length, 0);
-        done();
-      })
+    driver.wait(driver.findElement(By.css("title")).then(
+      function(webElement) {
+        console.log("title exists");
+      },
+      function(err) {
+        throw new Error("title does not exist");
+      }
+    ), 10000)
+    .then(function() {
+      AxeBuilder(driver)
+        .analyze(function(results) {
+          console.log(`Accessibility Violations: ${results.violations.length}`);
+          if (results.violations.length > 0) {
+            results.violations.forEach((violation, index) => {
+              console.log(`${index + 1}. ${violation.help}`);
+            });
+          }
+          assert.equal(results.violations.length, 0);
+          done();
+        });
+      // done();
+    });
   });
+    
+  it('some other test', function(done) {
+    console.log("Hello");
+    done();
+  });
+  
+});
 
-  it('should find violations', function(done) {
-    AxeBuilder(driver)
-      .withRules('html-has-lang')
-      .analyze(function(results) {
-        if (results.violations.length > 0) {
-          results.violations.forEach((violation, index) => {
-            console.log(`${index + 1}. ${violation.help}`);
-          });
-        }
-        assert.equal(results.passes.length, 1);
-        done();
-      });
-  });
+
+
+
+
+// driver.get(url).then(function() {
+//   done();
+// });
+
+// driver.get(url).then(function() {
+//   return driver.wait(function() {
+//     return driver.findElements(By.tagName("h1"));
+//   }, 10000)
+//   .then(function() {
+//     console.log("h1 present");
+//     done();
+//   });
+// });
+
+// driver.get(url).then(function() {
+//   AxeBuilder(driver)
+//     .analyze(function(results) {
+//       console.log(`Accessibility Violations: ${results.violations.length}`);
+//       if (results.violations.length > 0) {
+//         results.violations.forEach((violation, index) => {
+//           console.log(`${index + 1}. ${violation.help}`);
+//         });
+//       }
+//       assert.equal(results.violations.length, 0);
+//       done();
+//     })
+// });
+
+// it('title should exist', function(done) {
+//   // driver.wait(function() {
+//   //   driver.findElements(By.css("title")).then(function() {
+//   //     console.log("title exists");
+//   //     done();
+//   //   });
+//   // }, 10000);
+//   
+//   // driver.findElements(By.css("title")).then(
+//   //   function(webElements) {
+//   //     if (webElements.length) {
+//   //       webElements[0].getText().then(function(text) {
+//   //         console.log("Title found: " + text);
+//   //         assert(text);
+//   //         done();
+//   //       });
+//   //     } else {
+//   //       assert.fail(1, 1, "Title not found");
+//   //       done();
+//   //     }
+//   //   },
+//   //   function(err) {
+//   //     assert.fail(1, 1, "Title not found");
+//   //     done();
+//   //   }
+//   // );
+// });
+  
+  // it('title should exist', function(done) {
+  //   driver.wait(driver.findElement(By.css("title")), 10000).then(
+  //     function(webElement) {
+  //       console.log("title exists");
+  //       webElement.getText().then(function(text) {
+  //         console.log("title = " + text);
+  //         done();
+  //       });
+  //     },
+  //     function(err) {
+  //       if (err.state && err.state === "no such element") {
+  //         console.log("title does not exist");
+  //         done();
+  //       } else {
+  //         // promise.rejected(err);
+  //         done();
+  //       }
+  //     }
+  //   );
+  // });
+    
+    // AxeBuilder(driver)
+    //   .analyze(function(results) {
+    //     console.log(`Accessibility Violations: ${results.violations.length}`);
+    //     if (results.violations.length > 0) {
+    //       results.violations.forEach((violation, index) => {
+    //         console.log(`${index + 1}. ${violation.help}`);
+    //       });
+    //     }
+    //     assert.equal(results.violations.length, 0);
+    //     done();
+    //   });
+  
+  
+  
+  
+  
+  
+  // it('should find violations', function(done) {
+  //   AxeBuilder(driver)
+  //     .withRules('html-has-lang')
+  //     .analyze(function(results) {
+  //       if (results.violations.length > 0) {
+  //         results.violations.forEach((violation, index) => {
+  //           console.log(`${index + 1}. ${violation.help}`);
+  //         });
+  //       }
+  //       assert.equal(results.passes.length, 1);
+  //       done();
+  //     });
+  // });
   
   // xit('should change state with the keyboard', function() {
   //   var selector = 'span[role="radio"][aria-labelledby="radiogroup-0-label-0"]';
@@ -75,4 +213,17 @@ describe('Accessibility', function() {
   //     });
   // });
 
-});
+  // function isItThere(driver, element) {
+  //   driver.findElement(By.id(element)).then(
+  //     function(webElement) {
+  //       console.log(element + ' exists');
+  //     },
+  //     function(err) {
+  //       if (err.state && err.state === 'no such element') {
+  //         console.log(element + ' not found');
+  //       } else {
+  //         promise.rejected(err);
+  //       }
+  //     }
+  //   );
+  // }
